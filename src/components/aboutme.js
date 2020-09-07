8be2792918f47f67da68c9c1c51d8018ebd33891
Carter Keith <carterkeith3gmail.com>
2020-09-07 16:51:15 -0400

    third commit

---
+++ src/components/aboutme.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class About extends Component {
+  render() {
+    return(
+      <div>
+        <h1>About Page</h1>
+        <h2> Lorem ipsum</h2>
+
+        
+        </div>
+    )
+  }
+}
+
+export default About;
